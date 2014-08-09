fa1975e9812da25e7a4145ef2fd9be6efc473eab
ゆきねこ <@H_MobilePC>
2014-08-09 17:29:29 +0900

    IamHiroyuki

--- src/arcircle/Slick2D/ShootingMain/Main.java
+++ src/arcircle/Slick2D/ShootingMain/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-        System.out.println("HelloWorld");
+        System.out.println("Hello_My_World");
 
         //エントリーポイント
         AppGameContainer app;
