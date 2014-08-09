aae4761ed2ca4c86ba1e2d0cdcf55c9ca28a3883
俊樹 <@133.10.236.92>
2014-08-09 14:50:07 +0900

    i am inai

--- src/arcircle/Slick2D/ShootingMain/Main.java
+++ src/arcircle/Slick2D/ShootingMain/Main.java
@@ -7,6 +7,9 @@ import org.newdawn.slick.SlickException;
 public class Main {
 
 	public static void main(String[] args) {
+
+        System.out.println("HelloWorld");
+
         //エントリーポイント
         AppGameContainer app;
         try{
@@ -18,5 +21,6 @@ public class Main {
         }catch(SlickException e){
             e.printStackTrace();
         }
+
 	}
 }
